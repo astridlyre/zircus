fe6455db771491617ad5e6f645f52ff1f474ad8f
Erin Burton <eb@flat9.dev>
2021-06-28 23:17:48 -0700

    zircus Mon June 28 2021 @ 23:17:48

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,1 +1,17 @@
 export const q = x => document.getElementById(x)
+
+export const e = (type, classes, children) => {
+    const el = document.createElement(type)
+    el.classList.add(classes)
+
+    if (typeof children === 'string') {
+        el.innerText = children
+    } else if (children === undefined) {
+        return el
+    } else {
+        children.forEach(child => {
+            el.appendChild(child)
+        })
+    }
+    return el
+}
