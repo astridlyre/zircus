f8024f00699ce4542836c47345599c3fe877b441
Erin Burton <eb@flat9.dev>
2021-07-01 18:57:36 -0700

    Thu July 01 2021 @ 18:57:36

--- assets/js/product.js
+++ assets/js/product.js
@@ -103,7 +103,7 @@ import { cart } from "./cart.js"
 
         // Show fullsize pic
         viewFull() {
-            this.bigImage.src = this.item.images[3]
+            this.bigImage.src = this.item.images[2]
             this.bigImageEl.style.display = "flex"
         }
 
