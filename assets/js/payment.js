e05c57bdde0088ebf98b59f93306bc16ab04e053
Erin Burton <eb@flat9.dev>
2021-07-10 15:23:05 -0700

    zircus Sat July 10 2021 @ 15:23:05

--- assets/js/payment.js
+++ assets/js/payment.js
@@ -1,40 +1,42 @@
-import { q, API_ENDPOINT, state, Element } from "./utils.js"
-const stripe = Stripe("pk_test_51J93KzDIzwSFHzdzCZtyRcjMvw8em0bhnMrVmkBHaMFHuc2nkJ156oJGNxuz0G7W4Jx0R6OCy2nBXYTt6U8bSYew00PIAPcntP");
+import { q, API_ENDPOINT, state, Element } from './utils.js'
+const stripe = Stripe(
+    'pk_test_51J93KzDIzwSFHzdzCZtyRcjMvw8em0bhnMrVmkBHaMFHuc2nkJ156oJGNxuz0G7W4Jx0R6OCy2nBXYTt6U8bSYew00PIAPcntP'
+)
 
 export const checkout = (() => {
-    const placeOrder = q("place-order")
+    const placeOrder = q('place-order')
     if (!placeOrder) return
     if (state.cart.length === 0) location.assign('/')
 
     class Checkout {
         constructor() {
-            this.name = q("checkout-name")
-            this.email = q("checkout-email")
-            this.streetAddress = q("checkout-street")
-            this.city = q("checkout-city")
-            this.state = q("checkout-state")
-            this.stateText = q("checkout-state-text")
-            this.country = q("checkout-country")
-            this.zip = q("checkout-zip")
+            this.name = q('checkout-name')
+            this.email = q('checkout-email')
+            this.streetAddress = q('checkout-street')
+            this.city = q('checkout-city')
+            this.state = q('checkout-state')
+            this.stateText = q('checkout-state-text')
+            this.country = q('checkout-country')
+            this.zip = q('checkout-zip')
             this.zipText = q('checkout-zip-text')
-            this.nav = q("nav")
-            this.blur = q("blur")
-            this.paymentModal = q("stripe-payment-modal")
-            this.paymentForm = q("stripe-payment-form")
-            this.payBtn = q("pay-button")
-            this.cardError = q("card-error")
-            this.spinner = q("spinner")
-            this.btnText = q("button-text")
+            this.nav = q('nav')
+            this.blur = q('blur')
+            this.paymentModal = q('stripe-payment-modal')
+            this.paymentForm = q('stripe-payment-form')
+            this.payBtn = q('pay-button')
+            this.cardError = q('card-error')
+            this.spinner = q('spinner')
+            this.btnText = q('button-text')
             this.placeOrder = q('place-order')
             this.checkoutForm = q('checkout-form')
             this.cancel = q('cancel')
-            this.checkoutForm.addEventListener("submit", event => {
+            this.checkoutForm.addEventListener('submit', event => {
                 event.preventDefault()
                 this.createPaymentIntent()
             })
             this.cancel.addEventListener('click', () => this.showModal(false))
             // Add functionality for Countries States and Cities
-            state.addHook((s) => {
+            state.addHook(s => {
                 this.populateSelects(
                     this.country,
                     Object.keys(s.countries),
@@ -42,25 +44,24 @@ export const checkout = (() => {
                 )
                 this.handleCountry()
             })
-            this.country.addEventListener("input", () => this.handleCountry())
+            this.country.addEventListener('input', () => this.handleCountry())
         }
 
         showModal(show) {
             this.loading(false)
             if (show) {
                 document.body.classList.add('hide-y')
-                this.nav.classList.add("blur")
-                this.blur.classList.add("blur")
-                this.paymentModal.classList.add("show-modal")
+                this.nav.classList.add('blur')
+                this.blur.classList.add('blur')
+                this.paymentModal.classList.add('show-modal')
                 this.payBtn.disabled = true
                 q('payment-price').innerText = q('cart-total').innerText
             } else {
                 document.body.classList.remove('hide-y')
-                this.nav.classList.remove("blur")
-                this.blur.classList.remove("blur")
-                this.paymentModal.classList.remove("show-modal")
-                if (state.cart.length === 0)
-                    location.assign('/')
+                this.nav.classList.remove('blur')
+                this.blur.classList.remove('blur')
+                this.paymentModal.classList.remove('show-modal')
+                if (state.cart.length === 0) location.assign('/')
             }
         }
 
@@ -75,7 +76,7 @@ export const checkout = (() => {
                 state: this.state.value,
                 country: this.country.value,
                 zip: this.zip.value,
-                items: state.cart.map((item) => ({
+                items: state.cart.map(item => ({
                     type: item.type,
                     prefix: item.prefix,
                     size: item.size,
@@ -86,43 +87,45 @@ export const checkout = (() => {
             }
 
             fetch(`${API_ENDPOINT}/orders/create-payment-intent`, {
-                method: "POST",
+                method: 'POST',
                 headers: {
-                    "Content-Type": "application/json",
+                    'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(cart),
             })
-                .then((data) => data.json())
-                .then((data) => this.loadStripe(data))
+                .then(data => data.json())
+                .then(data => this.loadStripe(data))
         }
 
         loadStripe(data) {
             const elements = stripe.elements()
             const style = {
                 base: {
-                    color: "#1a171b",
-                    fontFamily: "Nunito, sans-serif",
-                    fontSmoothing: "antialiased",
-                    fontSize: "16px",
-                    "::placeholder": {
-                        color: "#1a171b",
+                    color: '#1a171b',
+                    fontFamily: 'Nunito, sans-serif',
+                    fontSmoothing: 'antialiased',
+                    fontSize: '16px',
+                    '::placeholder': {
+                        color: '#1a171b',
                     },
                 },
                 invalid: {
-                    fontFamily: "Nunito, sans-serif",
-                    color: "#fa755a",
-                    iconColor: "#fa755a",
+                    fontFamily: 'Nunito, sans-serif',
+                    color: '#fa755a',
+                    iconColor: '#fa755a',
                 },
             }
 
-            const card = elements.create("card", { style: style })
-            card.mount("#card-element")
-            card.on("change", (event) => {
+            const card = elements.create('card', { style: style })
+            card.mount('#card-element')
+            card.on('change', event => {
                 this.payBtn.disabled = event.empty
-                this.cardError.textContent = event.error ? event.error.message : ""
+                this.cardError.textContent = event.error
+                    ? event.error.message
+                    : ''
             })
 
-            this.paymentForm.addEventListener("submit", (event) => {
+            this.paymentForm.addEventListener('submit', event => {
                 event.preventDefault()
                 this.payWithCard(stripe, card, data.clientSecret)
             })
@@ -134,8 +137,9 @@ export const checkout = (() => {
                 .confirmCardPayment(clientSecret, {
                     payment_method: { card },
                 })
-                .then((result) => {
-                    if (result.error) return this.showError(result.error.message)
+                .then(result => {
+                    if (result.error)
+                        return this.showError(result.error.message)
                     return this.orderComplete(result.paymentIntent.id)
                 })
         }
@@ -144,32 +148,32 @@ export const checkout = (() => {
             this.loading(false)
             this.cardError.textContent = msg
             setTimeout(() => {
-                this.cardError.textContent = ""
+                this.cardError.textContent = ''
             }, 4000)
         }
 
         loading(isLoading) {
             if (isLoading) {
                 this.payBtn.disabled = true
-                this.spinner.classList.remove("hidden")
-                this.btnText.classList.add("hidden")
+                this.spinner.classList.remove('hidden')
+                this.btnText.classList.add('hidden')
             } else {
                 this.payBtn.disabled = false
-                this.spinner.classList.add("hidden")
-                this.btnText.classList.remove("hidden")
+                this.spinner.classList.add('hidden')
+                this.btnText.classList.remove('hidden')
             }
         }
 
         async orderComplete(id) {
             this.loading(false)
             return await fetch(`${API_ENDPOINT}/orders`, {
-                method: "POST",
+                method: 'POST',
                 headers: {
-                    "Content-Type": "application/json",
+                    'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({ id }),
             })
-                .then((res) => res.json())
+                .then(res => res.json())
                 .then(() => {
                     state.cart = () => []
                     q('result-message').classList.remove('hidden')
@@ -182,11 +186,13 @@ export const checkout = (() => {
 
         populateSelects(select, data = [], fn) {
             select.textContent = ''
-            data.forEach((item) => {
+            data.forEach(item => {
                 select.appendChild(
-                    new Element("option", null, {
+                    new Element('option', null, {
                         value: fn(item),
-                    }).addChild(fn(item)).render()
+                    })
+                        .addChild(fn(item))
+                        .render()
                 )
             })
         }
@@ -194,9 +200,15 @@ export const checkout = (() => {
         handleCountry() {
             const country = this.country.value
             this.state.textContent = ''
-            this.populateSelects(this.state, state.countries[country].states, item => item.name)
-            this.stateText.innerText = this.country.value === 'Canada' ? 'Province' : 'State'
-            this.zipText.innerText = this.country.value === 'Canada' ? 'Postal Code' : 'Zip'
+            this.populateSelects(
+                this.state,
+                state.countries[country].states,
+                item => item.name
+            )
+            this.stateText.innerText =
+                this.country.value === 'Canada' ? 'Province' : 'State'
+            this.zipText.innerText =
+                this.country.value === 'Canada' ? 'Postal Code' : 'Zip'
         }
     }
 
