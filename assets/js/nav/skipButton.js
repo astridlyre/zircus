d11caf4b890125ea1ea63909a33d207d8ebeecb1
Erin Burton <eb@flat9.dev>
2021-08-17 21:53:20 -0700

    zircus Tue August 17 2021 @ 21:53:20

--- assets/js/nav/skipButton.js
+++ assets/js/nav/skipButton.js
@@ -1,16 +1,61 @@
-import { ZircusElement } from "../utils.js";
+const Template = {
+  render() {
+    return `${this.html()}
+            ${this.css()}`;
+  },
+  html() {
+    return `
+    <button></button>
+    `;
+  },
+  css() {
+    return `
+    <style>
+      button {
+        position: fixed;
+        top: -120%;
+        left: 0;
+        z-index: 1000;
+        border-bottom-right-radius: var(--radius);
+        background: var(--light);
+        color: var(--dark);
+        outline: none;
+        border: 2px solid transparent;
+        transition: all 0.1s ease-out;
+        padding: var(--base-unit) var(--base-spacing);
+        font-size: var(--small-font-size);
+        font-family: var(--site-font-family);
+        font-weight: 600;
+        letter-spacing: var(--letter-spacing);
+      }
+
+      button:hover {
+        cursor: pointer;
+      }
+
+      button:focus {
+        top: 0;
+        transition: all 0.1s ease-out;
+        border-color: var(--teal);
+      }
+    </style>
+    `;
+  },
+};
 
 export default class ZircusSkipButton extends HTMLElement {
   #button;
 
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = Template.render();
+    this.#button = this.shadowRoot.querySelector("button");
+    this.#button.addEventListener("click", this.focusMain);
+  }
+
   connectedCallback() {
-    this.#button = new ZircusElement("button", [
-      "skip-to-content",
-      "small-spaced-bold",
-    ], {
-      title: this.getAttribute("text"),
-    }).event("click", this.focusMain).render();
-    this.appendChild(this.#button);
+    this.#button.setAttribute("title", this.getAttribute("text"));
     this.#button.textContent = this.getAttribute("text");
   }
 
--- assets/js/nav/toTopButton.js
+++ assets/js/nav/toTopButton.js
@@ -1,13 +1,89 @@
 import { eventBus } from "../utils.js";
 
+const Template = {
+  render() {
+    return `${this.html()}
+            ${this.css()}`;
+  },
+  html() {
+    return `
+<button>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="icon"
+  >
+    <polyline points="17 11 12 6 7 11"></polyline>
+    <polyline points="17 18 12 13 7 18"></polyline>
+  </svg>
+</button>`;
+  },
+  css() {
+    return `
+<style>
+  button {
+    opacity: 0.5;
+    padding: var(--base-unit);
+    transform: translateY(15px);
+    background: var(--gray-40);
+    visibility: hidden;
+    border: none;
+    border-radius: var(--radius);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    outline: none;
+    transition: transform 0.2s ease-out;
+    position: fixed;
+    bottom: var(--base-spacing);
+    right: calc(var(--x-padding) - 0.6rem);
+    z-index: 40;
+  }
+
+  @media screen and (min-width: 901px) {
+    button {
+      bottom: var(--md-spacing);
+      right: var(--md-spacing);
+    }
+  }
+
+  button:focus,
+  button:hover {
+    opacity: 1;
+    cursor: pointer;
+  }
+
+  button.show {
+    visibility: unset;
+    transform: translateY(0);
+    transition: transform 0.2s ease-out;
+  }
+
+  svg {
+    animation: 3s infinite ease-out slowPulse;
+    height: 1.55rem;
+    width: 1.55rem;
+  }
+</style>`;
+  },
+};
+
 export default class ZircusToTopButton extends HTMLElement {
   #MIN_SCROLL = 400;
   #button;
   #isHidden = true;
   #isUpdating = false;
 
-  connectedCallback() {
-    this.#button = this.querySelector("#to-top-button");
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = Template.render();
+    this.#button = this.shadowRoot.querySelector("button");
     this.#button.addEventListener("click", () => {
       window.scroll({ top: 0 });
       this.#button.blur();
