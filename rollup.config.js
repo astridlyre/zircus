60c1316637a246e6c61cccde99ab6e8daaa2563c
Erin Burton <eb@flat9.dev>
2021-08-18 19:49:29 -0700

    zircus Wed August 18 2021 @ 19:49:29

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,31 @@
-import resolve from '@rollup/plugin-node-resolve'
-import { terser } from 'rollup-plugin-terser'
-import { babel } from '@rollup/plugin-babel'
-import commonjs from '@rollup/plugin-commonjs'
+import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+import { babel } from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-    input: 'assets/js/index.js',
-    output: {
-        file: 'assets/js/bundle.js',
-        format: 'esm',
-    },
-    plugins: [
-        resolve(),
-        commonjs(),
-        terser(),
-        babel({
-            babelHelpers: 'bundled',
-            exclude: 'node_modules/**',
-            presets: [
-                [
-                    '@babel/preset-env',
-                    {
-                        corejs: '3.16.1',
-                        useBuiltIns: 'usage',
-                        targets: '> 5%, last 2 versions',
-                    },
-                ],
-            ],
-        }),
-    ],
-}
+  input: "assets/js/index.js",
+  output: {
+    file: "assets/js/bundle.js",
+    format: "esm",
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    terser(),
+    babel({
+      babelHelpers: "bundled",
+      exclude: "node_modules/**",
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            corejs: "3.16.1",
+            useBuiltIns: "usage",
+            targets: "> 5% in CA, last 2 versions",
+          },
+        ],
+      ],
+    }),
+  ],
+};
